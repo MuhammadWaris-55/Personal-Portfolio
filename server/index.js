97515ca90687f6d6b688400185b39e5189df5cb3
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-09 00:39:46 +0500

    Added PORT in env file

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.post("/send-email", async (req, res) => {
     }
 });
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
